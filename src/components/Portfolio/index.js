2a252f745c243113eb0fbfc1ac145cb1041e3067
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-04-17 20:02:30 -0400

    added 6 websites to the portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import 'bootstrap'
-import reciperepo from './assets/images/recipe-repo.png'
+import 'bootstrap';
+import reciperepo from './assets/images/recipe-repo.png';
+import socialMediaAPI from './assets/images/social-media-API.png';
+import employeedatabase from './assets/images/employee-database.png';
+import Ecommerce from './assets/images/e-commerce.jpg';
+import techblog from './assets/images/techblog.png';
+import weather from './assets/images/weather-dashboard.png';
 
 function Portfolio({ currentCategory }) {
 
@@ -10,58 +15,58 @@ function Portfolio({ currentCategory }) {
       <div className="container-md">
         <div className="row">
           <div className="col-md-4 content-spacing">
-            <h3>Recipe Repo</h3>
+            <h4>Recipe Repo (Full-Stack (SQL))</h4>
             <img src={reciperepo} alt='recipe-repo website. Find the best recipes online' />
             <div className="row justify-content-evenly border-radius link-box">
               <a className="btn btn-info col-sm-4" href="https://damp-crag-63651.herokuapp.com/"><h6>Web</h6></a>
-              <a className="btn btn-info col-sm-4 " href="https://github.com/reggietenk/Recipe-Repo-.git"><h6>Git</h6></a>
+              <a className="btn btn-info col-sm-4 " href="https://github.com/Zachsteuer98/Recipe-Repo-.git"><h6>Git</h6></a>
             </div>
           </div>
           <br></br>
           <div className="col-md-4 content-spacing">
-            <h3>Recipe Repo</h3>
-            <img src={reciperepo} alt='recipe-repo website. Find the best recipes online' />
+            <h4>Social Media API (MongoDB)</h4>
+            <img src={socialMediaAPI} alt='This is a social media API using Express.js, Mongoose, and MongoDB' />
             <div className="row justify-content-evenly border-radius link-box">
-              <a className="btn btn-info col-sm-4" href="https://damp-crag-63651.herokuapp.com/"><h6>Web</h6></a>
-              <a className="btn btn-info col-sm-4 " href="https://github.com/reggietenk/Recipe-Repo-.git"><h6>Git</h6></a>
+              <a className="btn btn-info col-sm-4" href="https://drive.google.com/file/d/11t2EZgzmwpPozgxF63pwPrKX74YtwNnq/view"><h6>Video</h6></a>
+              <a className="btn btn-info col-sm-4" href="https://github.com/Zachsteuer98/social-media-API.git"><h6>Git</h6></a>
             </div>
           </div>
           <br></br>
           <div className="col-md-4 content-spacing">
-            <h3>Recipe Repo</h3>
-            <img src={reciperepo} alt='recipe-repo website. Find the best recipes online' />
+            <h4>Employee Database (SQL)</h4>
+            <img src={employeedatabase} alt='recipe-repo website. Find the best recipes online' />
             <div className="row justify-content-evenly border-radius link-box">
-              <a className="btn btn-info col-sm-4" href="https://damp-crag-63651.herokuapp.com/">Website</a>
-              <a className="btn btn-info col-sm-4 " href="https://github.com/reggietenk/Recipe-Repo-.git">Github</a>
+              <a className="btn btn-info col-sm-4" href="https://drive.google.com/file/d/1hcxXvlao6V4I8K4C6dFYvDckfysrpF0C/view"><h6>Video</h6></a>
+              <a className="btn btn-info col-sm-4 " href="https://github.com/Zachsteuer98/Employee-database.git"><h6>Git</h6></a>
             </div>
           </div>
         </div>
         <br></br>
         <div className="row">
-        <div className="col-md-4 content-spacing">
-            <h3>Recipe Repo</h3>
-            <img src={reciperepo} alt='recipe-repo website. Find the best recipes online' />
+          <div className="col-md-4 content-spacing">
+            <h4>E-Commerce API (SQL)</h4>
+            <img src={Ecommerce} alt='E-Commerce Backend Utilizing SQL Database and Express Servers' />
             <div className="row justify-content-evenly border-radius link-box">
-              <a className="btn btn-info col-sm-4" href="https://damp-crag-63651.herokuapp.com/">Website</a>
-              <a className="btn btn-info col-sm-4 " href="https://github.com/reggietenk/Recipe-Repo-.git">Github</a>
+              <a className="btn btn-info col-sm-4" href="https://drive.google.com/file/d/1yAj7VdDoZNyKIn6ozq0kToOhA-t7T81-/view">Video</a>
+              <a className="btn btn-info col-sm-4 " href="https://github.com/Zachsteuer98/E-Commerce-Backend.git">Git</a>
             </div>
           </div>
           <br></br>
           <div className="col-md-4 content-spacing">
-            <h3>Recipe Repo</h3>
-            <img src={reciperepo} alt='recipe-repo website. Find the best recipes online' />
+            <h4>Weather Dashboard (OpenWeather API)</h4>
+            <img src={weather} alt='weather dashboard page' />
             <div className="row justify-content-evenly border-radius link-box">
-              <a className="btn btn-info col-sm-4" href="https://damp-crag-63651.herokuapp.com/">Website</a>
-              <a className="btn btn-info col-sm-4 " href="https://github.com/reggietenk/Recipe-Repo-.git">Github</a>
+              <a className="btn btn-info col-sm-4" href="https://zachsteuer98.github.io/Weather-Appliction/">Web</a>
+              <a className="btn btn-info col-sm-4 " href="https://github.com/Zachsteuer98/Weather-Appliction.git">Git</a>
             </div>
           </div>
           <br></br>
           <div className="col-md-4 content-spacing">
-            <h3>Recipe Repo</h3>
-            <img src={reciperepo} alt='recipe-repo website. Find the best recipes online' />
+            <h4>Tech Blog</h4>
+            <img src={techblog} alt='Tech blog Website' />
             <div className="row justify-content-evenly border-radius link-box">
-              <a className="btn btn-info col-sm-4" href="https://damp-crag-63651.herokuapp.com/">Website</a>
-              <a className="btn btn-info col-sm-4 " href="https://github.com/reggietenk/Recipe-Repo-.git">Github</a>
+              <a className="btn btn-info col-sm-4" href="https://immense-ridge-93447.herokuapp.com/">Web</a>
+              <a className="btn btn-info col-sm-4 " href="https://github.com/Zachsteuer98/Tech-Blog.git">Git</a>
             </div>
           </div>
         </div>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import resume from './assets/images/resume.png'
+import resumedoc from './assets/files/resume.docx'
 
-function Resume () {
+function Resume() {
 
     return (
         <section>
-            <div><h1>
-            </h1> Resume </div>
+            <div className="margin-bottom">
+                <div>
+                <div className="row justify-content-evenly">
+                    <img src={resume} alt="resume for Zachary Steuer" className="resumeImage"></img>
+                </div>
+                <div className="justify-content-evenly">
+                </div>
+                <button className="btn btn-info col-sm-4 download-button" ><a href={resumedoc}>Download Resume</a></button>
+                </div>
+            </div>
         </section>
     )
 }
